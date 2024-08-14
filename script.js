b6ec3ed87e55c0d4d358c1e2d29c70251bed3b60
Sagunthala A <117196512+Sagunthala-A@users.noreply.github.com>
2024-08-14 09:06:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,31 @@
-//your JS code here. If required.
-const divElem = document.getElementsByClassName("square");
+// Function to create elements with a specific tag and class
+function createEle(tagNam, classNam) {
+  let ele = document.createElement(tagNam);
+  ele.setAttribute('class', classNam);
+  return ele;
+}
 
+// Create a container and append it to the body
+const container = createEle('div', 'container');
+document.body.appendChild(container);
 
+// Function to generate a random color value
 function randomColor() {
-    return Math.floor(Math.random() * 255);
-  // const randomColorGen = "#" + Math.floor(Math.random() * 16777215).toString(16);
-  // return randomColorGen
-}
-for(let i =0; i< divElem.length; i++){
-  
-	divElem[i].addEventListener('mouseenter', () => {
-    // divElem[i].style.backgroundColor =  randomColorGen();
-    divElem[i].style.backgroundColor = `rgba(${randomColor()}, ${randomColor()}, ${randomColor()}, 1)`;
-    
-    
-    
-})
-  divElem[i].addEventListener('mouseleave',()=>{
-    divElem[i].style.backgroundColor = 'rgb(29, 29, 29)'
-    divElem[i].style.transition = 'background-color 1s linear'
-  })
+  return Math.floor(Math.random() * 255);
 }
 
+// Create 800 square elements, append them to the container, and add event listeners
+for (let i = 0; i < 800; i++) {
+  const divElem = createEle('div', 'square');
+  container.append(divElem);
+
+  divElem.addEventListener('mouseenter', () => {
+    divElem.style.backgroundColor = `rgba(${randomColor()}, ${randomColor()}, ${randomColor()}, 1)`;
+    divElem.style.transition = '';
+  });
+
+  divElem.addEventListener('mouseleave', () => {
+    divElem.style.backgroundColor = 'rgb(29, 29, 29)';
+    divElem.style.transition = 'background-color 1s linear';
+  });
+}
