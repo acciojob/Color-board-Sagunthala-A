d7e54413657ca86f4248c80a43718edacac8d4c4
Sagunthala A <117196512+Sagunthala-A@users.noreply.github.com>
2024-08-14 08:30:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your JS code here. If required.
+const divElem = document.getElementsByClassName("square");
+
+
+function randomColor() {
+    return Math.floor(Math.random() * 255);
+  // const randomColorGen = "#" + Math.floor(Math.random() * 16777215).toString(16);
+  // return randomColorGen
+}
+for(let i =0; i< divElem.length; i++){
+  
+	divElem[i].addEventListener('mouseenter', () => {
+    // divElem[i].style.backgroundColor =  randomColorGen();
+    divElem[i].style.backgroundColor = `rgba(${randomColor()}, ${randomColor()}, ${randomColor()}, 1)`;
+    
+    
+    
+})
+  divElem[i].addEventListener('mouseleave',()=>{
+    divElem[i].style.backgroundColor = 'transparent';
+    divElem[i].style.transition = 'background-color 1s linear'
+  })
+}
+
