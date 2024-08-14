49fa91002d242fe64eafa4f09d07675e9920dba5
Sagunthala A <117196512+Sagunthala-A@users.noreply.github.com>
2024-08-14 09:16:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 // Function to create elements with a specific tag and class
-document.addEventListener('DOMContentLoaded', () => {
 function createEle(tagNam, classNam) {
   let ele = document.createElement(tagNam);
   ele.setAttribute('class', classNam);
@@ -15,19 +14,27 @@ function randomColor() {
   return Math.floor(Math.random() * 255);
 }
 
+function changeColor(element) {
+    
+    element.style.backgroundColor = `rgba(${randomColor()}, ${randomColor()}, ${randomColor()}, 1)`;
+
+    setTimeout(() => {
+        element.style.backgroundColor = 'rgb(29,29,29)';
+    }, 1000); // Revert after 1 second
+}
+
 // Create 800 square elements, append them to the container, and add event listeners
 for (let i = 0; i < 800; i++) {
   const divElem = createEle('div', 'square');
   container.append(divElem);
 
   divElem.addEventListener('mouseenter', () => {
-    divElem.style.backgroundColor = `rgba(${randomColor()}, ${randomColor()}, ${randomColor()}, 1)`;
-    divElem.style.transition = '';
+    changeColor(divElem)
+    // divElem.style.backgroundColor = `rgba(${randomColor()}, ${randomColor()}, ${randomColor()}, 1)`;
+    
   });
 
-  divElem.addEventListener('mouseleave', () => {
-    divElem.style.backgroundColor = 'rgb(29, 29, 29)';
-    divElem.style.transition = 'background-color 1s linear';
-  });
-}
+
+  
+  
 }
